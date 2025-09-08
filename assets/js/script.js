cf45cd2071ec352e356195bc27fca77cdf4dfeef
Radley-code <radleyacha00@icloud.com>
2025-09-08 12:28:53 +0100

    Add mobile menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Mobile menu from html by using its id
 const mobileMenu = document.getElementById('mobile-menu');
 mobileMenu.addEventListener('click', () => { //add event listener to the mobile menu
-    const desktopMenu = document.getElementById('desktop-menu'); // grabs the desktop menu
+    const desktopMenu = document.getElementById('desktop-menu'); // gets the desktop menu
     // Swap class mames of mobile and desktop menus
     mobileMenu.classList.toggle('active');
     desktopMenu.classList.toggle('active');
